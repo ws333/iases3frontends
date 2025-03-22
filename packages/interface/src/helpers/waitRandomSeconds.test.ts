2b0a0cbbc3a6d6b37ce4b3ab0d160b36ef5b77db
Bjørnar Hvidsten <devenania@proton.me>
2025-03-22 22:53:31 +0100

    Make waitRandomSeconds testable again

--- packages/interface/src/helpers/waitRandomSeconds.test.ts
+++ packages/interface/src/helpers/waitRandomSeconds.test.ts
@@ -7,15 +7,14 @@ for (let i = 0; i < numberOfIterations; i++) {
     test("expect wait to keep within time window, negative minSeconds", async () => {
         const minSeconds = -0.3;
         const randomWindow = 0.2;
-        const ms = await waitRandomSeconds(minSeconds, randomWindow);
+        const { timeout } = await waitRandomSeconds(minSeconds, randomWindow);
         const expectMinSeconds = Math.abs(minSeconds) * 1000;
-        const expectMaxSeconds =
-            Math.abs(minSeconds) * 1000 + Math.abs(randomWindow) * 1000;
-        console.log("*Debug* -> test -> ms:", ms);
+        const expectMaxSeconds = Math.abs(minSeconds) * 1000 + Math.abs(randomWindow) * 1000;
+        console.log("*Debug* -> test -> ms:", timeout);
         console.log("*Debug* -> test -> expectMinSeconds:", expectMinSeconds);
         console.log("*Debug* -> test -> expectMaxSeconds:", expectMaxSeconds);
-        expect(ms).toBeGreaterThanOrEqual(expectMinSeconds);
-        expect(ms).toBeLessThanOrEqual(expectMaxSeconds);
+        expect(timeout).toBeGreaterThanOrEqual(expectMinSeconds);
+        expect(timeout).toBeLessThanOrEqual(expectMaxSeconds);
     });
 }
 
@@ -23,15 +22,14 @@ for (let i = 0; i < numberOfIterations; i++) {
     test("expect wait to keep within time window, negative randomWindow", async () => {
         const minSeconds = 0.3;
         const randomWindow = -0.2;
-        const ms = await waitRandomSeconds(minSeconds, randomWindow);
+        const { timeout } = await waitRandomSeconds(minSeconds, randomWindow);
         const expectMinSeconds = Math.abs(minSeconds) * 1000;
-        const expectMaxSeconds =
-            Math.abs(minSeconds) * 1000 + Math.abs(randomWindow) * 1000;
-        console.log("*Debug* -> test -> ms:", ms);
+        const expectMaxSeconds = Math.abs(minSeconds) * 1000 + Math.abs(randomWindow) * 1000;
+        console.log("*Debug* -> test -> ms:", timeout);
         console.log("*Debug* -> test -> expectMinSeconds:", expectMinSeconds);
         console.log("*Debug* -> test -> expectMaxSeconds:", expectMaxSeconds);
-        expect(ms).toBeGreaterThanOrEqual(expectMinSeconds);
-        expect(ms).toBeLessThanOrEqual(expectMaxSeconds);
+        expect(timeout).toBeGreaterThanOrEqual(expectMinSeconds);
+        expect(timeout).toBeLessThanOrEqual(expectMaxSeconds);
     });
 }
 
@@ -39,15 +37,14 @@ for (let i = 0; i < numberOfIterations; i++) {
     test("expect wait to keep within time window, negative minSeconds and negative randomWindow", async () => {
         const minSeconds = -0.3;
         const randomWindow = -0.2;
-        const ms = await waitRandomSeconds(minSeconds, randomWindow);
+        const { timeout } = await waitRandomSeconds(minSeconds, randomWindow);
         const expectMinSeconds = Math.abs(minSeconds) * 1000;
-        const expectMaxSeconds =
-            Math.abs(minSeconds) * 1000 + Math.abs(randomWindow) * 1000;
-        console.log("*Debug* -> test -> ms:", ms);
+        const expectMaxSeconds = Math.abs(minSeconds) * 1000 + Math.abs(randomWindow) * 1000;
+        console.log("*Debug* -> test -> ms:", timeout);
         console.log("*Debug* -> test -> expectMinSeconds:", expectMinSeconds);
         console.log("*Debug* -> test -> expectMaxSeconds:", expectMaxSeconds);
-        expect(ms).toBeGreaterThanOrEqual(expectMinSeconds);
-        expect(ms).toBeLessThanOrEqual(expectMaxSeconds);
+        expect(timeout).toBeGreaterThanOrEqual(expectMinSeconds);
+        expect(timeout).toBeLessThanOrEqual(expectMaxSeconds);
     });
 }
 
@@ -55,15 +52,14 @@ for (let i = 0; i < numberOfIterations; i++) {
     test("expect wait to keep within time window, minSeconds 0", async () => {
         const minSeconds = 0;
         const randomWindow = 0.4;
-        const ms = await waitRandomSeconds(minSeconds, randomWindow);
+        const { timeout } = await waitRandomSeconds(minSeconds, randomWindow);
         const expectMinSeconds = Math.abs(minSeconds) * 1000;
-        const expectMaxSeconds =
-            Math.abs(minSeconds) * 1000 + Math.abs(randomWindow) * 1000;
-        console.log("*Debug* -> test -> ms:", ms);
+        const expectMaxSeconds = Math.abs(minSeconds) * 1000 + Math.abs(randomWindow) * 1000;
+        console.log("*Debug* -> test -> ms:", timeout);
         console.log("*Debug* -> test -> expectMinSeconds:", expectMinSeconds);
         console.log("*Debug* -> test -> expectMaxSeconds:", expectMaxSeconds);
-        expect(ms).toBeGreaterThanOrEqual(expectMinSeconds);
-        expect(ms).toBeLessThanOrEqual(expectMaxSeconds);
+        expect(timeout).toBeGreaterThanOrEqual(expectMinSeconds);
+        expect(timeout).toBeLessThanOrEqual(expectMaxSeconds);
     });
 }
 
@@ -71,14 +67,13 @@ for (let i = 0; i < numberOfIterations; i++) {
     test("expect wait to keep within time window, randomWindow 0", async () => {
         const minSeconds = 0.3;
         const randomWindow = 0;
-        const ms = await waitRandomSeconds(minSeconds, randomWindow);
+        const { timeout } = await waitRandomSeconds(minSeconds, randomWindow);
         const expectMinSeconds = Math.abs(minSeconds) * 1000;
-        const expectMaxSeconds =
-            Math.abs(minSeconds) * 1000 + Math.abs(randomWindow) * 1000;
-        console.log("*Debug* -> test -> ms:", ms);
+        const expectMaxSeconds = Math.abs(minSeconds) * 1000 + Math.abs(randomWindow) * 1000;
+        console.log("*Debug* -> test -> ms:", timeout);
         console.log("*Debug* -> test -> expectMinSeconds:", expectMinSeconds);
         console.log("*Debug* -> test -> expectMaxSeconds:", expectMaxSeconds);
-        expect(ms).toBeGreaterThanOrEqual(expectMinSeconds);
-        expect(ms).toBeLessThanOrEqual(expectMaxSeconds);
+        expect(timeout).toBeGreaterThanOrEqual(expectMinSeconds);
+        expect(timeout).toBeLessThanOrEqual(expectMaxSeconds);
     });
 }
