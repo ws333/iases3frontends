dd58c6a56d4bd1f3df5f063b190db5b7890f8a16
Bjørnar Hvidsten <devenania@proton.me>
2025-03-22 17:19:03 +0100

    Refactor zeroWidtSpace

--- packages/interface/src/constants/constants.ts
+++ packages/interface/src/constants/constants.ts
@@ -20,3 +20,5 @@ export const sessionFinishedText = "Session finished!";
 // Used to send to just one specified contact per session, i.e. disable the use of the contacts list.
 // export const SINGLE_CONTACT_MODE = true;
 export const SINGLE_CONTACT_MODE = import.meta.env.VITE_SINGLE_CONTACT_MODE === "1";
+
+export const zeroWidtSpace = "\u200b";
