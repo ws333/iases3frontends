964d6756814d6645b58820dbaeb640518182bf86
Bjørnar Hvidsten <devenania@proton.me>
2025-03-25 13:04:14 +0100

    Make header not wrap in add-on

--- packages/interface/src/components/Header.tsx
+++ packages/interface/src/components/Header.tsx
@@ -1,9 +1,5 @@
 function Header() {
-    return (
-        <h1 className="header-title">
-            Interstellar Alliance Social Experiment Step 3
-        </h1>
-    );
+    return <h1 className="header-title no-wrap">Interstellar Alliance Social Experiment Step 3</h1>;
 }
 
 export default Header;
