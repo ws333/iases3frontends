6c86be9a5de65a0bdf9da0613b9ca40cd3aaed83
Bjørnar Hvidsten <devenania@proton.me>
2025-04-01 19:04:10 +0200

    Improve TextDeletingData styling

--- packages/interface/src/components/dialogTexts/TextDeletingData.tsx
+++ packages/interface/src/components/dialogTexts/TextDeletingData.tsx
@@ -2,7 +2,7 @@ import { Button, Text } from "radzionkit";
 import { CSSProperties } from "styled-components";
 import { exportFromLocalStorage } from "../../helpers/exportFromLocalStorage";
 
-const onClickButton = () => {
+const onClickExportButton = () => {
     exportFromLocalStorage();
 };
 
@@ -12,22 +12,20 @@ const styles: CSSProperties = {
 
 const TextDeletingData = (
     <>
-        <Text color="supporting" style={styles}>
-            You are about to delete all sending history!
-        </Text>
-        <Text color="supporting" style={styles}>
+        <Text style={styles}>You are about to delete all sending history!</Text>
+        <Text style={styles}>
             This means that information about last sending date and sending count to individual contacts will be
             removed.
         </Text>
-        <Text color="supporting" style={styles}>
+        <Text style={styles}>
             If you intend to continue sending later or on another computer make sure to export the data first!
         </Text>
-        <Button onClick={onClickButton} style={{ margin: "1rem" }}>
-            Export data
-        </Button>
-        <Text color="regular" style={{ ...styles, textAlign: "center" }}>
-            Are you sure you want to delete the sending history?
-        </Text>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+            <Button onClick={onClickExportButton} style={{ margin: "1rem", width: "60%" }}>
+                Export data
+            </Button>
+        </div>
+        <Text style={{ ...styles, textAlign: "center" }}>Are you sure you want to delete the sending history?</Text>
     </>
 );
 
