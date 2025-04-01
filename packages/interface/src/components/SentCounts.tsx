21144fbc5fc0663f9e4ca3ed83e2a3cb98499a22
Bjørnar Hvidsten <devenania@proton.me>
2025-04-01 18:04:16 +0200

    Add email sending count stats tooltip

--- packages/interface/src/components/SentCounts.tsx
+++ packages/interface/src/components/SentCounts.tsx
@@ -1,9 +1,8 @@
 import { VStack } from "@lib/ui/css/stack";
 import { ExpandablePanel } from "@lib/ui/panel/ExpandablePanel";
+import { Tooltip } from "radzionkit/ui/tooltips/Tooltip";
 import { UseContactListReturnType } from "../hooks/useContactList";
 import "./SentCounts.css";
-
-// Add this import for custom styles
 
 type Props = {
     useCL: UseContactListReturnType;
@@ -14,25 +13,31 @@ export function SentCounts({ useCL }: Props) {
         <ExpandablePanel
             className="sent-counts-panel"
             header={
-                <VStack alignItems="start" fullWidth={true}>
-                    <div className="container_header_sent_counts">
-                        <div className="column_left_header_sent_counts">
-                            <div>Emails sent last 24 hours</div>
-                        </div>
-                        <div className="column_right_header_sent_counts">
-                            <div>{useCL.totalSentCount24Hours}</div>
-                        </div>
-                    </div>
-                </VStack>
+                <Tooltip
+                    placement="top"
+                    renderOpener={(props) => (
+                        <VStack {...props} alignItems="start" fullWidth={true}>
+                            <div className="container_header_sent_counts">
+                                <div className="column_left_header_sent_counts">
+                                    <div>Emails sent last 24 hours</div>
+                                </div>
+                                <div className="column_right_header_sent_counts">
+                                    <div>{useCL.totalSentCount24Hours}</div>
+                                </div>
+                            </div>
+                        </VStack>
+                    )}
+                    content={"Click to see more/less stats"}
+                />
             }
             renderContent={() => (
                 <div className="container_sent_counts">
                     <div className="column_left_sent_counts">
                         <div>Total sent count</div>
                         <div>Last hour</div>
-                        <div>7 days</div>
-                        <div>30 days</div>
-                        <div>3 months</div>
+                        <div>Last 7 days</div>
+                        <div>Last 30 days</div>
+                        <div>Last 3 months</div>
                     </div>
                     <div className="column_right_sent_counts">
                         <div>{useCL.totalSentCount}</div>
