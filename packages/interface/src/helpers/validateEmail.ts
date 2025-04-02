7e0572854a587d350debc8524bf57e0592b747fd
Bjørnar Hvidsten <devenania@proton.me>
2025-04-02 13:00:44 +0200

    Improve typings and default return values for IndexedDb helpers
    Function getLastImportExportDate returns 0 instead of undefined if key  lastImportExportDate doesn't exist, return -1 if error.
    
    Clean up a few comments

--- packages/interface/src/helpers/validateEmail.ts
+++ packages/interface/src/helpers/validateEmail.ts
@@ -1,5 +1,4 @@
 export const validateEmail = (email: string): boolean => {
-    // Best choice for email validation according to Grok 3 beta and ChatGPT-4o
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return emailRegex.test(email);
 };
