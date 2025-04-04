4030919c45caeff4640fa2a4ddcb0626308505cb
Bjørnar Hvidsten <devenania@proton.me>
2025-04-04 13:03:54 +0200

    Edit custom subjects

--- packages/interface/src/constants/emailTemplates.ts
+++ packages/interface/src/constants/emailTemplates.ts
@@ -5,12 +5,9 @@ export const subjects: Subjects = {
     English: [
         "The Interstellar Alliance Social Experiment Group",
         "Public Disclosure of UFO/UAP Information",
-        "Call for Transparency on UFOs and Extraterrestrial Evidence",
-        "End UFO secrets Now, begin public education",
-        "Petition for Immediate UFO/UAP Disclosure within One year",
-        "A plea for Openness: UFOs Pose No Threat",
-        "Time for Truth: Release All UFO/UAP Data",
-        "Interstellar Alliance: UFO Secrets Must End Now",
+        "Call for Transparency on UFO/UAP and Extraterrestrial presence",
+        "End UFO secrets now, begin public education",
+        "Petition for UFO/UAP Disclosure within One year",
         "Custom Subject",
     ],
     Norwegian: [
