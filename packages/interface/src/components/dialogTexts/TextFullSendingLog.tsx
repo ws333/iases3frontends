d3bdcb89215baa1777138c7d89f59d58bdecbac3
Bjørnar Hvidsten <devenania@proton.me>
2025-04-04 12:38:14 +0200

    Adjust text and styling of dialogs
    Update readzionkit after adjusting styling of Modal title and ModalCloseButton

--- packages/interface/src/components/dialogTexts/TextFullSendingLog.tsx
+++ packages/interface/src/components/dialogTexts/TextFullSendingLog.tsx
@@ -12,7 +12,7 @@ const TextFullSendingLog = (fullSendingLog: string[]) => {
     return (
         <>
             <Text size={11} color="alert" style={{ ...styles, paddingLeft: "1.5rem" }}>
-                - Note that this view is not updated while sending! -
+                - Note that this view is not updated while sending! Press the esc or space key to close this window -
             </Text>
             {fullSendingLog.map((log, index) => (
                 <Text size={11} key={index} style={styles}>
