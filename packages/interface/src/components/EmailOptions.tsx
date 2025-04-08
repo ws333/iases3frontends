ws333/MailMergeP

Move emailOptions state into easy-peasy model
Store subject per language

packages/interface/src/components/EmailOptions.tsx
@@ -3,35 +3,43 @@ import { FocusEvent, useEffect, useRef, useState } from "react";
 import { maxCountOptions, minSendingDelay } from "../constants/constants";
 import { KeyOfEmailComponents, emailComponents, subjects } from "../constants/emailTemplates";
 import { useStoreActions, useStoreState } from "../hooks/storeHooks";
-import { useEmailOptions } from "../hooks/useEmailOptions";
 import { storeOptionsKey } from "../helpers/indexedDB";
 import { objectKeys } from "../helpers/objectHelpers";
 import "./EmailOptions.css";
 
 export type EmailOptionsProps = {
-    emailOptions: ReturnType<typeof useEmailOptions>;
     isSending: boolean;
     singleContactMode: boolean;
 };
 
-function EmailOptions({ emailOptions, isSending, singleContactMode }: EmailOptionsProps) {
+function EmailOptions({ isSending, singleContactMode }: EmailOptionsProps) {
+    const delay = useStoreState((state) => state.emailOptions.delay);
+    const setDelay = useStoreActions((actions) => actions.emailOptions.setDelay);
+
+    const language = useStoreState((state) => state.emailOptions.language);
+    const setLanguage = useStoreActions((actions) => actions.emailOptions.setLanguage);
+
+    const subject = useStoreState((state) => state.emailOptions.subject);
+    const setSubjectOption = useStoreActions((actions) => actions.emailOptions.setSubjectPerLanguage);
+
+    const customSubject = useStoreState((state) => state.emailOptions.customSubject);
+    const setCustomSubject = useStoreActions((actions) => actions.emailOptions.setCustomSubject);
+
     const maxCount = useStoreState((state) => state.contactList.maxCount);
     const setMaxCount = useStoreActions((actions) => actions.contactList.setMaxCount);
 
     const customSubjectRef = useRef<HTMLInputElement>(null);
     if (customSubjectRef.current) {
-        customSubjectRef.current.value = emailOptions.customSubject;
+        customSubjectRef.current.value = customSubject;
     }
+    const customSubjectVisible = useStoreState((state) => state.emailOptions.customSubjectVisible);
 
     const onBlurCustomSubject = (e: FocusEvent<HTMLInputElement>) => {
         if (customSubjectRef.current) {
             customSubjectRef.current.value = e.target.value;
         }
-        emailOptions.setCustomSubject(e.target.value);
+        setCustomSubject(e.target.value);
     };
-
-    const customSubjectVisible =
-        emailOptions.subjectOption === "Custom Subject" || emailOptions.subjectOption === "Tilpasset Emne"; // TODO:Use last items in the subjects array for these strings
 
     useEffect(() => {
         if (customSubjectVisible) {
@@ -42,11 +50,11 @@ function EmailOptions({ emailOptions, isSending, singleContactMode }: EmailOptio
 
     // Update local delay after hydration
     useEffect(() => {
-        setLocalDelay(emailOptions.delay.toString());
-    }, [emailOptions.delay]);
+        setLocalDelay(delay.toString());
+    }, [delay]);
 
     // Using local state to allow empty input
-    const [localDelay, setLocalDelay] = useState(emailOptions.delay.toString());
+    const [localDelay, setLocalDelay] = useState(delay.toString());
 
     function processDelayInput(value: string) {
         const delay = Number(value);
@@ -79,7 +87,7 @@ function EmailOptions({ emailOptions, isSending, singleContactMode }: EmailOptio
                                             onBlur={(e) => {
                                                 const delay = processDelayInput(e.target.value);
                                                 setLocalDelay(delay.toString());
-                                                emailOptions.setDelay(delay);
+                                                setDelay(delay);
                                             }}
                                         />
                                     </div>
@@ -120,9 +128,9 @@ function EmailOptions({ emailOptions, isSending, singleContactMode }: EmailOptio
                 Email language
                 <br />
                 <select
-                    value={emailOptions.language}
+                    value={language}
                     disabled={isSending}
-                    onChange={(e) => emailOptions.setLanguage(e.target.value as KeyOfEmailComponents)}
+                    onChange={(e) => setLanguage({ language: e.target.value as KeyOfEmailComponents })}
                 >
                     {objectKeys(emailComponents).map((_language) => (
                         <option key={_language} value={_language}>
@@ -137,11 +145,11 @@ function EmailOptions({ emailOptions, isSending, singleContactMode }: EmailOptio
                     Subject
                     <br />
                     <select
-                        value={emailOptions.subjectOption}
+                        value={subject}
                         disabled={isSending}
-                        onChange={(e) => emailOptions.setSubjectOption(e.target.value)}
+                        onChange={(e) => setSubjectOption({ [language]: e.target.value })}
                     >
-                        {subjects[emailOptions.language].map((_subject) => (
+                        {subjects[language].map((_subject) => (
                             <option key={_subject} value={_subject}>
                                 {_subject}
                             </option>

packages/interface/src/hooks/useEmailOptions.ts
@@ -1,57 +1,29 @@
-import { useCallback, useEffect, useRef, useState } from "react";
-import { TEmailComponent } from "../types/types";
+import { useEffect } from "react";
 import { defaultMaxCount, defaultSendingDelay } from "../constants/constants";
-import { KeyOfEmailComponents, defaultLanguage, emailComponents, subjects } from "../constants/emailTemplates";
-import LetterEnglish from "../components/letters/LetterEnglish";
-import { OPTIONS_KEY, getOptions, storeOptionsKey } from "../helpers/indexedDB";
+import { KeyOfEmailComponents, defaultLanguage, subjects } from "../constants/emailTemplates";
+import { OPTIONS_KEY, getOptions } from "../helpers/indexedDB";
 import { useStoreActions, useStoreState } from "./storeHooks";
 
 function useEmailOptions() {
-    const [delay, _setDelay] = useState<number>(defaultSendingDelay);
-    const [language, _setLanguage] = useState<KeyOfEmailComponents>(defaultLanguage);
-    const [subjectOption, _setSubjectOption] = useState<string>(subjects[language][0]);
-    const [customSubject, _setCustomSubject] = useState<string>("");
+    const delay = useStoreState((state) => state.emailOptions.delay);
+    const setDelay = useStoreActions((actions) => actions.emailOptions.setDelay);
+
+    const language = useStoreState((state) => state.emailOptions.language);
+    const setLanguage = useStoreActions((actions) => actions.emailOptions.setLanguage);
+
+    const subjectOption = useStoreState((state) => state.emailOptions.subjectPerLanguage);
+
+    const customSubject = useStoreState((state) => state.emailOptions.customSubject);
+    const setCustomSubject = useStoreActions((actions) => actions.emailOptions.setCustomSubject);
+
+    const EmailComponent = useStoreState((state) => state.emailOptions.EmailComponent);
+
     const selectedSubject =
-        subjectOption === "Custom Subject" || subjectOption === "Tilpasset Emne" ? customSubject : subjectOption;
+        subjectOption[language] === "Custom Subject" || subjectOption[language] === "Tilpasset Emne"
+            ? customSubject
+            : subjectOption[language];
 
     const setMaxCount = useStoreActions((actions) => actions.contactList.setMaxCount);
-    const setNationOptions = useStoreActions((actions) => actions.contactList.setNationOptions);
-    const nationOptionsFetched = useStoreState((state) => state.contactList.nationOptionsFetched);
-
-    const EmailComponentRef = useRef<TEmailComponent>(LetterEnglish);
-
-    const setDelay = (value: number) => {
-        storeOptionsKey(value, "delay");
-        _setDelay(value);
-    };
-
-    const setLanguage = useCallback(
-        (value: KeyOfEmailComponents, subject?: string) => {
-            storeOptionsKey(value, "language");
-            _setLanguage(value);
-            if (value === "Norwegian") {
-                setNationOptions(["NO"]);
-            } else {
-                setNationOptions(nationOptionsFetched);
-            }
-            const _subject = subject ?? subjects[value][0];
-            storeOptionsKey(_subject, "subject");
-            _setSubjectOption(_subject);
-
-            EmailComponentRef.current = emailComponents[value];
-        },
-        [nationOptionsFetched, setNationOptions]
-    );
-
-    const setSubjectOption = (value: string) => {
-        storeOptionsKey(value, "subject");
-        _setSubjectOption(value);
-    };
-
-    const setCustomSubject = (value: string) => {
-        storeOptionsKey(value, "customSubject");
-        _setCustomSubject(value);
-    };
 
     useEffect(() => {
         const hydrateOptions = async () => {
@@ -59,7 +31,7 @@ function useEmailOptions() {
             if (options) {
                 const delay = (options.find((item) => item.key === OPTIONS_KEY.DELAY)?.value ??
                     defaultSendingDelay) as number;
-                _setDelay(delay);
+                setDelay(delay);
 
                 const _maxCount = (options.find((item) => item.key === OPTIONS_KEY.MAX_COUNT)?.value ??
                     defaultMaxCount) as number;
@@ -67,29 +39,25 @@ function useEmailOptions() {
 
                 const customSubject = (options.find((item) => item.key === OPTIONS_KEY.CUSTOM_SUBJECT)?.value ??
                     "") as string;
-                _setCustomSubject(customSubject);
+                setCustomSubject(customSubject);
 
                 const language = (options.find((item) => item.key === OPTIONS_KEY.LANGUAGE)?.value ??
                     defaultLanguage) as KeyOfEmailComponents;
-                const subject = (options.find((item) => item.key === OPTIONS_KEY.SUBJECT)?.value ??
-                    subjects[language][0]) as string;
-                setLanguage(language, subject);
+                const storedSubjectPerLanguage = options.find((item) => item.key === OPTIONS_KEY.SUBJECT)
+                    ?.value as string;
+                const subjectPerLanguage = storedSubjectPerLanguage
+                    ? JSON.parse(storedSubjectPerLanguage)
+                    : { [language]: subjects[language][0] };
+                setLanguage({ language, subjectPerLanguage: subjectPerLanguage });
             }
         };
         void hydrateOptions();
-    }, [setLanguage, setMaxCount]);
+    }, [setCustomSubject, setDelay, setLanguage, setMaxCount]);
 
     return {
         delay,
-        setDelay,
-        language,
-        setLanguage,
-        subjectOption,
-        setSubjectOption,
-        customSubject,
-        setCustomSubject,
         selectedSubject,
-        EmailComponent: EmailComponentRef.current,
+        EmailComponent,
     };
 }
 export { useEmailOptions };

packages/interface/src/types/modelTypes.ts
@@ -1,6 +1,7 @@
-import { Action, ActionOn, Computed, Thunk } from "easy-peasy";
+import { Action, ActionOn, Computed, Thunk, ThunkOn } from "easy-peasy";
 import { JSX } from "react";
-import type { SpreadsheetData, Strings } from "./types.ts";
+import { KeyOfEmailComponents, SubjectPerLanguage } from "../constants/emailTemplates.ts";
+import type { SpreadsheetData, Strings, TEmailComponent } from "./types.ts";
 import { ContactI3C } from "./typesI3C.ts";
 
 export interface Model {
@@ -16,6 +17,7 @@ export interface Model {
     openUrl: Thunk<Model, string>;
     userDialog: UserDialog;
     contactList: ContactList;
+    emailOptions: EmailOptions;
 }
 
 interface Locale {
@@ -86,6 +88,28 @@ interface ContactList {
     isSelectedAllNations: boolean;
     setIsSelectedAllNations: Action<ContactList, boolean>;
     toggleIsSelectedAllNations: Thunk<ContactList>;
+}
+
+interface EmailOptions {
+    delay: number;
+    setDelay: Action<EmailOptions, number>;
+    storeDelay: ThunkOn<EmailOptions, number>;
+
+    language: KeyOfEmailComponents;
+    setLanguage: Action<EmailOptions, { language: KeyOfEmailComponents; subjectPerLanguage?: SubjectPerLanguage }>;
+    storeLanguage: ThunkOn<EmailOptions, KeyOfEmailComponents, Model>;
+
+    subject: Computed<EmailOptions, string>;
+    subjectPerLanguage: SubjectPerLanguage;
+    setSubjectPerLanguage: Action<EmailOptions, SubjectPerLanguage>;
+    storeSubjectPerLanguage: ThunkOn<EmailOptions, SubjectPerLanguage>;
+
+    customSubject: string;
+    setCustomSubject: Action<EmailOptions, string>;
+    storeCustomSubject: ThunkOn<EmailOptions, string>;
+    customSubjectVisible: Computed<EmailOptions, boolean>;
+
+    EmailComponent: Computed<EmailOptions, TEmailComponent>;
 }
 
 export interface UserDialog {

ws333/iases3frontends

packages/interface/src/components/EmailOptions.tsx
@@ -1,37 +1,45 @@
 import { Tooltip } from "radzionkit/ui/tooltips/Tooltip";
 import { FocusEvent, useEffect, useRef, useState } from "react";
 import { maxCountOptions, minSendingDelay } from "../constants/constants";
-import { KeyOfEmailComponents, emailComponents, subjects } from "../constants/emailTemplates";
+import { LanguageOption, emailComponents, subjects } from "../constants/emailTemplates";
 import { useStoreActions, useStoreState } from "../hooks/storeHooks";
-import { useEmailOptions } from "../hooks/useEmailOptions";
 import { storeOptionsKey } from "../helpers/indexedDB";
 import { objectKeys } from "../helpers/objectHelpers";
 import "./EmailOptions.css";
 
 export type EmailOptionsProps = {
-    emailOptions: ReturnType<typeof useEmailOptions>;
     isSending: boolean;
     singleContactMode: boolean;
 };
 
-function EmailOptions({ emailOptions, isSending, singleContactMode }: EmailOptionsProps) {
+function EmailOptions({ isSending, singleContactMode }: EmailOptionsProps) {
+    const delay = useStoreState((state) => state.emailOptions.delay);
+    const setDelay = useStoreActions((actions) => actions.emailOptions.setDelay);
+
+    const language = useStoreState((state) => state.emailOptions.language);
+    const setLanguage = useStoreActions((actions) => actions.emailOptions.setLanguage);
+
+    const subject = useStoreState((state) => state.emailOptions.subject);
+    const setSubjectOption = useStoreActions((actions) => actions.emailOptions.setSubjectPerLanguage);
+
+    const customSubject = useStoreState((state) => state.emailOptions.customSubject);
+    const setCustomSubject = useStoreActions((actions) => actions.emailOptions.setCustomSubject);
+
     const maxCount = useStoreState((state) => state.contactList.maxCount);
     const setMaxCount = useStoreActions((actions) => actions.contactList.setMaxCount);
 
     const customSubjectRef = useRef<HTMLInputElement>(null);
     if (customSubjectRef.current) {
-        customSubjectRef.current.value = emailOptions.customSubject;
+        customSubjectRef.current.value = customSubject;
     }
+    const customSubjectVisible = useStoreState((state) => state.emailOptions.customSubjectVisible);
 
     const onBlurCustomSubject = (e: FocusEvent<HTMLInputElement>) => {
         if (customSubjectRef.current) {
             customSubjectRef.current.value = e.target.value;
         }
-        emailOptions.setCustomSubject(e.target.value);
+        setCustomSubject(e.target.value);
     };
-
-    const customSubjectVisible =
-        emailOptions.subjectOption === "Custom Subject" || emailOptions.subjectOption === "Tilpasset Emne"; // TODO:Use last items in the subjects array for these strings
 
     useEffect(() => {
         if (customSubjectVisible) {
@@ -42,11 +50,11 @@ function EmailOptions({ emailOptions, isSending, singleContactMode }: EmailOptio
 
     // Update local delay after hydration
     useEffect(() => {
-        setLocalDelay(emailOptions.delay.toString());
-    }, [emailOptions.delay]);
+        setLocalDelay(delay.toString());
+    }, [delay]);
 
     // Using local state to allow empty input
-    const [localDelay, setLocalDelay] = useState(emailOptions.delay.toString());
+    const [localDelay, setLocalDelay] = useState(delay.toString());
 
     function processDelayInput(value: string) {
         const delay = Number(value);
@@ -79,7 +87,7 @@ function EmailOptions({ emailOptions, isSending, singleContactMode }: EmailOptio
                                             onBlur={(e) => {
                                                 const delay = processDelayInput(e.target.value);
                                                 setLocalDelay(delay.toString());
-                                                emailOptions.setDelay(delay);
+                                                setDelay(delay);
                                             }}
                                         />
                                     </div>
@@ -120,9 +128,9 @@ function EmailOptions({ emailOptions, isSending, singleContactMode }: EmailOptio
                 Email language
                 <br />
                 <select
-                    value={emailOptions.language}
+                    value={language}
                     disabled={isSending}
-                    onChange={(e) => emailOptions.setLanguage(e.target.value as KeyOfEmailComponents)}
+                    onChange={(e) => setLanguage({ language: e.target.value as LanguageOption })}
                 >
                     {objectKeys(emailComponents).map((_language) => (
                         <option key={_language} value={_language}>
@@ -137,11 +145,11 @@ function EmailOptions({ emailOptions, isSending, singleContactMode }: EmailOptio
                     Subject
                     <br />
                     <select
-                        value={emailOptions.subjectOption}
+                        value={subject}
                         disabled={isSending}
-                        onChange={(e) => emailOptions.setSubjectOption(e.target.value)}
+                        onChange={(e) => setSubjectOption({ [language]: e.target.value })}
                     >
-                        {subjects[emailOptions.language].map((_subject) => (
+                        {subjects[language].map((_subject) => (
                             <option key={_subject} value={_subject}>
                                 {_subject}
                             </option>

packages/interface/src/hooks/useEmailOptions.ts
@@ -1,57 +1,29 @@
-import { useCallback, useEffect, useRef, useState } from "react";
-import { TEmailComponent } from "../types/types";
+import { useEffect } from "react";
 import { defaultMaxCount, defaultSendingDelay } from "../constants/constants";
-import { KeyOfEmailComponents, defaultLanguage, emailComponents, subjects } from "../constants/emailTemplates";
-import LetterEnglish from "../components/letters/LetterEnglish";
-import { OPTIONS_KEY, getOptions, storeOptionsKey } from "../helpers/indexedDB";
+import { LanguageOption, defaultLanguage, subjects } from "../constants/emailTemplates";
+import { OPTIONS_KEY, getOptions } from "../helpers/indexedDB";
 import { useStoreActions, useStoreState } from "./storeHooks";
 
 function useEmailOptions() {
-    const [delay, _setDelay] = useState<number>(defaultSendingDelay);
-    const [language, _setLanguage] = useState<KeyOfEmailComponents>(defaultLanguage);
-    const [subjectOption, _setSubjectOption] = useState<string>(subjects[language][0]);
-    const [customSubject, _setCustomSubject] = useState<string>("");
+    const delay = useStoreState((state) => state.emailOptions.delay);
+    const setDelay = useStoreActions((actions) => actions.emailOptions.setDelay);
+
+    const language = useStoreState((state) => state.emailOptions.language);
+    const setLanguage = useStoreActions((actions) => actions.emailOptions.setLanguage);
+
+    const subjectOption = useStoreState((state) => state.emailOptions.subjectPerLanguage);
+
+    const customSubject = useStoreState((state) => state.emailOptions.customSubject);
+    const setCustomSubject = useStoreActions((actions) => actions.emailOptions.setCustomSubject);
+
+    const EmailComponent = useStoreState((state) => state.emailOptions.EmailComponent);
+
     const selectedSubject =
-        subjectOption === "Custom Subject" || subjectOption === "Tilpasset Emne" ? customSubject : subjectOption;
+        subjectOption[language] === "Custom Subject" || subjectOption[language] === "Tilpasset Emne"
+            ? customSubject
+            : subjectOption[language];
 
     const setMaxCount = useStoreActions((actions) => actions.contactList.setMaxCount);
-    const setNationOptions = useStoreActions((actions) => actions.contactList.setNationOptions);
-    const nationOptionsFetched = useStoreState((state) => state.contactList.nationOptionsFetched);
-
-    const EmailComponentRef = useRef<TEmailComponent>(LetterEnglish);
-
-    const setDelay = (value: number) => {
-        storeOptionsKey(value, "delay");
-        _setDelay(value);
-    };
-
-    const setLanguage = useCallback(
-        (value: KeyOfEmailComponents, subject?: string) => {
-            storeOptionsKey(value, "language");
-            _setLanguage(value);
-            if (value === "Norwegian") {
-                setNationOptions(["NO"]);
-            } else {
-                setNationOptions(nationOptionsFetched);
-            }
-            const _subject = subject ?? subjects[value][0];
-            storeOptionsKey(_subject, "subject");
-            _setSubjectOption(_subject);
-
-            EmailComponentRef.current = emailComponents[value];
-        },
-        [nationOptionsFetched, setNationOptions]
-    );
-
-    const setSubjectOption = (value: string) => {
-        storeOptionsKey(value, "subject");
-        _setSubjectOption(value);
-    };
-
-    const setCustomSubject = (value: string) => {
-        storeOptionsKey(value, "customSubject");
-        _setCustomSubject(value);
-    };
 
     useEffect(() => {
         const hydrateOptions = async () => {
@@ -59,7 +31,7 @@ function useEmailOptions() {
             if (options) {
                 const delay = (options.find((item) => item.key === OPTIONS_KEY.DELAY)?.value ??
                     defaultSendingDelay) as number;
-                _setDelay(delay);
+                setDelay(delay);
 
                 const _maxCount = (options.find((item) => item.key === OPTIONS_KEY.MAX_COUNT)?.value ??
                     defaultMaxCount) as number;
@@ -67,29 +39,25 @@ function useEmailOptions() {
 
                 const customSubject = (options.find((item) => item.key === OPTIONS_KEY.CUSTOM_SUBJECT)?.value ??
                     "") as string;
-                _setCustomSubject(customSubject);
+                setCustomSubject(customSubject);
 
                 const language = (options.find((item) => item.key === OPTIONS_KEY.LANGUAGE)?.value ??
-                    defaultLanguage) as KeyOfEmailComponents;
-                const subject = (options.find((item) => item.key === OPTIONS_KEY.SUBJECT)?.value ??
-                    subjects[language][0]) as string;
-                setLanguage(language, subject);
+                    defaultLanguage) as LanguageOption;
+                const storedSubjectPerLanguage = options.find((item) => item.key === OPTIONS_KEY.SUBJECT)
+                    ?.value as string;
+                const subjectPerLanguage = storedSubjectPerLanguage
+                    ? JSON.parse(storedSubjectPerLanguage)
+                    : { [language]: subjects[language][0] };
+                setLanguage({ language, subjectPerLanguage: subjectPerLanguage });
             }
         };
         void hydrateOptions();
-    }, [setLanguage, setMaxCount]);
+    }, [setCustomSubject, setDelay, setLanguage, setMaxCount]);
 
     return {
         delay,
-        setDelay,
-        language,
-        setLanguage,
-        subjectOption,
-        setSubjectOption,
-        customSubject,
-        setCustomSubject,
         selectedSubject,
-        EmailComponent: EmailComponentRef.current,
+        EmailComponent,
     };
 }
 export { useEmailOptions };

packages/interface/src/types/modelTypes.ts
@@ -1,6 +1,7 @@
-import { Action, ActionOn, Computed, Thunk } from "easy-peasy";
+import { Action, ActionOn, Computed, Thunk, ThunkOn } from "easy-peasy";
 import { JSX } from "react";
-import type { SpreadsheetData, Strings } from "./types.ts";
+import { LanguageOption, SubjectPerLanguage } from "../constants/emailTemplates.ts";
+import type { SpreadsheetData, Strings, TEmailComponent } from "./types.ts";
 import { ContactI3C } from "./typesI3C.ts";
 
 export interface Model {
@@ -16,6 +17,7 @@ export interface Model {
     openUrl: Thunk<Model, string>;
     userDialog: UserDialog;
     contactList: ContactList;
+    emailOptions: EmailOptions;
 }
 
 interface Locale {
@@ -86,6 +88,28 @@ interface ContactList {
     isSelectedAllNations: boolean;
     setIsSelectedAllNations: Action<ContactList, boolean>;
     toggleIsSelectedAllNations: Thunk<ContactList>;
+}
+
+interface EmailOptions {
+    delay: number;
+    setDelay: Action<EmailOptions, number>;
+    storeDelay: ThunkOn<EmailOptions, number>;
+
+    language: LanguageOption;
+    setLanguage: Action<EmailOptions, { language: LanguageOption; subjectPerLanguage?: SubjectPerLanguage }>;
+    storeLanguage: ThunkOn<EmailOptions, LanguageOption, Model>;
+
+    subject: Computed<EmailOptions, string>;
+    subjectPerLanguage: SubjectPerLanguage;
+    setSubjectPerLanguage: Action<EmailOptions, SubjectPerLanguage>;
+    storeSubjectPerLanguage: ThunkOn<EmailOptions, SubjectPerLanguage>;
+
+    customSubject: string;
+    setCustomSubject: Action<EmailOptions, string>;
+    storeCustomSubject: ThunkOn<EmailOptions, string>;
+    customSubjectVisible: Computed<EmailOptions, boolean>;
+
+    EmailComponent: Computed<EmailOptions, TEmailComponent>;
 }
 
 export interface UserDialog {