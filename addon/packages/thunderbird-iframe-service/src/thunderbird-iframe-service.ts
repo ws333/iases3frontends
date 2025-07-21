73af3ad5f6674438085bb2dc0de3ee0a1e619c37
Bjørnar Hvidsten <devenania@proton.me>
2025-07-21 20:19:43 +0200

    Clean out obsolete code from Mail Merge P

--- addon/packages/thunderbird-iframe-service/src/thunderbird-iframe-service.ts
+++ addon/packages/thunderbird-iframe-service/src/thunderbird-iframe-service.ts
@@ -1,7 +1,7 @@
 /*
  * Provide a messaging api equivalent to what is supplied by Thunderbird when running as an extension
  */
-import { Email, Prefs } from "../../interface/src/types/modelTypes";
+import { Email } from "../../interface/src/types/modelTypes";
 import { getUniqueMessageId } from "../../interface/src/helpers/getUniqueMessageId";
 import { iframeService } from "../../iframe-service/src/iframe-service";
 
@@ -18,80 +18,8 @@ try {
         /*
          * Functions to simulate the mailmerge commands
          */
-        function getDefaultPreferences() {
-            return {
-                delay: 0,
-                sendmode: "now",
-                range: "",
-                fileName: "",
-                fileContents: [],
-            };
-        }
 
-        async function getPreferences() {
-            let prefs = getDefaultPreferences();
-            try {
-                const fetched = await browser.storage.local.get("prefs");
-                if (fetched.prefs) {
-                    prefs = fetched.prefs;
-                }
-            } catch (error) {
-                console.warn("error when loading prefs", error);
-            }
-            return prefs;
-        }
-
-        async function setPreferences(prefs: Prefs) {
-            const newPrefs = { ...(await getPreferences()), ...prefs };
-            await browser.storage.local.set({ prefs: newPrefs });
-        }
-
-        function getLocalizedStrings() {
-            const stringNames = [
-                "next",
-                "previous",
-                "cancel",
-                "send",
-                "data",
-                "dataInfo",
-                "openAFile",
-                "dataHeaderWarning",
-                "settings",
-                "preview",
-                "sendMode",
-                "sendModeDesc",
-                "sendModeNow",
-                "sendModeLater",
-                "sendModeDraft",
-                "messageDelay",
-                "messageDelayDesc",
-                "sendMessageRange",
-                "sendMessageRangeDesc",
-                "previewEmpty",
-                "previewPreviewing",
-                "about",
-                "developers",
-                "support",
-                "license",
-                "donate",
-                "euro",
-                "dollar",
-                "current",
-                "total",
-                "time",
-                "progress",
-                "status",
-                "sending",
-                "waiting",
-            ];
-            const ret: Record<string, string> = {};
-            for (const name of stringNames) {
-                ret[name] = browser.i18n.getMessage(name);
-            }
-            return ret;
-        }
-
-        async function sendEmail(email: Email, sendmode: Prefs["sendmode"]) {
+        async function sendEmail(email: Email) {
             // Sending messages in the background blocked until https://bugzilla.mozilla.org/show_bug.cgi?id=1545930 is resolved.
 
             // Create the email in a new compose window and then send it.
@@ -99,9 +27,7 @@ try {
 
             // There are theoretically more send options, but https://bugzilla.mozilla.org/show_bug.cgi?id=1747456 is the blocker.
             if (newWin.id) {
-                const sentStatus = await browser.compose.sendMessage(newWin.id, {
-                    mode: sendmode === "now" ? "sendNow" : "sendLater",
-                });
+                const sentStatus = await browser.compose.sendMessage(newWin.id);
 
                 // Broadcast status in case of failure or cancellation
                 iframeService.messageChild({
@@ -112,25 +38,10 @@ try {
             }
         }
 
-        function cancel() {
-            // If the cancel button was clicked, close the window
-            browser.runtime.sendMessage({ action: "close" });
-        }
-
-        function openUrl(url: string | URL) {
-            window.open(url, "_blank");
-        }
-
         // Attach all our function calls to the iframeService
-        // iframeService.log = function () {}; // Comment out if you want to see debug messages // Todo - comment this line out
+        // iframeService.log = function () {}; // Comment out if you want to see debug messages
         Object.assign(iframeService.commands, {
-            getDefaultPreferences,
-            getPreferences,
-            getLocalizedStrings,
-            setPreferences,
             sendEmail,
-            openUrl,
-            cancel,
         });
     })();
 } catch (e) {
