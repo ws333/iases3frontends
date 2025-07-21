cfd7f478ade6eec12a0bf3e5ed2ca59bbb99bc41
Bjørnar Hvidsten <devenania@proton.me>
2025-07-21 20:19:43 +0200

    Clean out obsolete code from Mail Merge P

--- addon/packages/browser-preview/src/browser-iframe-service.ts
+++ addon/packages/browser-preview/src/browser-iframe-service.ts
@@ -1,7 +1,7 @@
 /*
  * Provide a messaging api equivalent to what is supplied by Thunderbird when running as an extension
  */
-import { Email, Prefs } from "../../interface/src/types/modelTypes";
+import { Email } from "../../interface/src/types/modelTypes";
 import { Message, iframeService } from "../../iframe-service/src/iframe-service";
 
 if (typeof iframeService === "undefined") {
@@ -47,103 +47,15 @@ if (typeof iframeService === "undefined") {
     /*
      * Functions to simulate the mailmerge commands
      */
-    function getDefaultPreferences() {
-        return {
-            delay: 0,
-            sendmode: "now",
-            range: "",
-            fileName: "",
-            fileContents: [],
-        };
-    }
 
-    function getPreferences() {
-        let prefs = getDefaultPreferences();
-        try {
-            const storedPrefs = window.localStorage.getItem("prefs");
-            prefs = storedPrefs ? JSON.parse(storedPrefs) : prefs;
-        } catch (error) {
-            console.warn("error when decoding prefs from JSON", error);
-        }
-        return prefs;
-    }
-
-    function setPreferences(prefs: Prefs) {
-        const newPrefs = { ...getPreferences(), ...prefs };
-        window.localStorage.setItem("prefs", JSON.stringify(newPrefs));
-    }
-
-    function getLocalizedStrings() {
-        return {
-            next: "Next",
-            previous: "Previous",
-            cancel: "Cancel",
-            send: "Send",
-            data: "Data",
-            dataInfo:
-                "Open a spreadsheet file (.csv, .xlsx, .ods, etc.) or copy-and-paste data into the spreadsheet below.",
-            dataHeaderWarning:
-                "The first row of the data act as keys for your email template. The keys must be single words without spaces or special characters.",
-            openAFile: "Open a file...",
-            settings: "Settings",
-            preview: "Preview",
-            sendMode: "Send mode:",
-            sendModeDesc: "Set how messages will be delivered. Send Later will leave messages in the Drafts folder.",
-            sendModeNow: "Send Now",
-            sendModeLater: "Send Later",
-            sendModeDraft: "Save as Draft",
-            messageDelay: "Message delay:",
-            messageDelayDesc: "Dealy, in seconds, between sending messages.",
-            sendMessageRange: "Send Message Range:",
-            sendMessageRangeDesc: "Send only specific messages as specified by this range.",
-            previewEmpty: "No emails to preview. Try loading data in the {0} tab.",
-            previewPreviewing: "Previewing {0} of {1}",
-            about: "About",
-            developers: "Developers",
-            support: "Support",
-            license: "License",
-            donate: "Donate",
-            euro: "{0} €",
-            dollar: "$ {0}",
-            current: "Current:",
-            total: "Total:",
-            time: "Time:",
-            progress: "Progress:",
-            status: "Status:",
-            sending: "Sending...",
-            waiting: "Waiting...",
-        };
-    }
-
-    function sendEmails(emails: Email[]) {
-        for (const email of emails) {
-            console.log("%c Sending Email", "background: blue; color: white;", email);
-        }
-    }
-
-    function sendEmail(email: Email, sendmode: Prefs["sendmode"]) {
-        console.log("%c Sending Email", "background: purple; color: white;", email, sendmode);
-    }
-
-    function cancel() {
-        alert("Cancel pressed");
-    }
-
-    function openUrl(url: string | URL) {
-        window.open(url, "_blank");
+    function sendEmail(email: Email) {
+        console.log("%c Sending Email", "background: purple; color: white;", email);
     }
 
     // Attach all our function calls to the iframeService
     iframeService.log = log;
     Object.assign(iframeService.commands, {
-        getDefaultPreferences,
-        getPreferences,
-        getLocalizedStrings,
-        setPreferences,
-        sendEmails,
         sendEmail,
-        openUrl,
-        cancel,
     });
 })();
 
