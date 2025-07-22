b77bdea6ecea15cffcc75d00c5083c306bccdffa
Bjørnar Hvidsten <devenania@proton.me>
2025-07-22 20:03:27 +0200

    Convert to only one pnpm workspace

--- webapp/frontend/vite.config.ts
+++ webapp/frontend/vite.config.ts
@@ -18,8 +18,6 @@ export default defineConfig({
       '@lib/ui': path.resolve(__dirname, './node_modules/ui-kit/lib/ui'),
       '@lib/utils': path.resolve(__dirname, './node_modules/ui-kit/lib/utils'),
       '@lib/codegen': path.resolve(__dirname, './node_modules/ui-kit/lib/codegen'),
-      // Force resolve styled-components to a single instance to avoid multiple instances warning
-      'styled-components': path.resolve(__dirname, 'node_modules/styled-components'),
     },
   },
   build: {
