0c6357c4e56ca9f668b73af0e847675c806e3e7d
Bjørnar Hvidsten <devenania@proton.me>
2025-07-25 14:58:48 +0200

    Update linting rules webapp

--- webapp/frontend/src/main.tsx
+++ webapp/frontend/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { Root } from './components/Root';
 import './main.css';
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Root />
