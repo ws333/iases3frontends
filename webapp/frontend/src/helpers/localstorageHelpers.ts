7369aa30e2e135e4bca1e8d1c8de7f295d007653
Bjørnar Hvidsten <devenania@proton.me>
2025-07-27 15:37:07 +0200

    Add buttons for active logins
    Improve and refactor Header and make it sticky
    Rename and move InfoComponent
    Refactor usage of useMsal
    Refactor some styles

--- webapp/frontend/src/helpers/localstorageHelpers.ts
+++ webapp/frontend/src/helpers/localstorageHelpers.ts
@@ -1,4 +1,4 @@
-import { LastLoginButtonValue, LoginGoogleInProgress } from '../types/types';
+import { LastLoginButtonValue, LoginDebounceMeasurements, LoginGoogleInProgress } from '../types/types';
 
 /**
  * Persist state to allow automatic login to last chosen login provider if already authenticated
@@ -37,4 +37,25 @@ export function setLoginGoogleInProgress(value: LoginGoogleInProgress) {
 
 export function removeLoginGoogleInProgress() {
   localStorage.removeItem(loginGoogleInProgressKey);
+}
+
+/**
+ * Persist and retrieve login debounce measurements for cross-session statistics
+ */
+
+const loginDebounceMeasurementsKey = 'loginDebounceMeasurements';
+
+export function getLoginDebounceMeasurements(maxMeasurements: number): LoginDebounceMeasurements {
+  const stored = localStorage.getItem(loginDebounceMeasurementsKey);
+  if (stored) {
+    const arr = JSON.parse(stored);
+    if (Array.isArray(arr) && arr.every((n) => typeof n === 'number')) {
+      return arr.slice(-maxMeasurements);
+    }
+  }
+  return [];
+}
+
+export function setLoginDebounceMeasurements(arr: LoginDebounceMeasurements) {
+  localStorage.setItem(loginDebounceMeasurementsKey, JSON.stringify(arr));
 }
