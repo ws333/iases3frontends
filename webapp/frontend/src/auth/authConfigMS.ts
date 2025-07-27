ws333/MailMergeIASE

Add Google oauth support
Refactor to handle both MS and Google login
Add hooks and localstorage state to handle authentication flow
Set root path to /webapp
Set protected path to /webapp/authorized
Refactor constants

webapp/frontend/src/auth/authConfigMS.ts
@@ -1,8 +1,5 @@
 import { Configuration, LogLevel, PopupRequest, RedirectRequest } from '@azure/msal-browser';
-import { PATH_WEBAPP, SERVER_DEV, SERVER_PROD } from '../constants/constants';
-
-const redirectUri = import.meta.env.PROD ? `${SERVER_PROD}/${PATH_WEBAPP}` : `${SERVER_DEV}/${PATH_WEBAPP}`;
-export const postLogoutRedirectUri = import.meta.env.PROD ? SERVER_PROD : SERVER_DEV;
+import { POST_LOGOUT_REDIRECT_URI, REDIRECT_URI } from '../constants/constantsImportMeta';
 
 /**
  * Configuration object to be passed to MSAL instance on creation.
@@ -13,8 +10,8 @@ export const msalConfig: Configuration = {
   auth: {
     clientId: '3b48599d-a38e-4b59-9504-cbff748b9cc0',
     authority: 'https://login.microsoftonline.com/common',
-    redirectUri: redirectUri, // Points to window.location.origin. You must register this URI on Microsoft Entra admin center/App Registration.
-    postLogoutRedirectUri: postLogoutRedirectUri, // Indicates the page to navigate after logout. You must register this URI on Microsoft Entra admin center/App Registration.
+    redirectUri: REDIRECT_URI, // You must register this URI on Microsoft Extra ID admin center/App Registration.
+    postLogoutRedirectUri: POST_LOGOUT_REDIRECT_URI, // Indicates the page to navigate after logout. You must register this URI on Microsoft Entra admin center/App Registration.
     navigateToLoginRequestUrl: false, // If "true", will navigate back to the original request location before processing the auth code response.
   },
   cache: {

webapp/frontend/src/components/GoogleLogOut.tsx
@@ -1,0 +1,27 @@
+import { googleLogout } from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
+import { useStoreActions } from '../../../../addon/packages/interface/src/hooks/storeHooks';
+import { removeLastLoginButtonClicked } from '../helpers/localstorageHelpers';
+import { revokeSessionGoogle } from '../auth/revokeSessionGoogle';
+
+const GoogleLogOut = () => {
+  const resetCurrentLogin = useStoreActions((state) => state.auth.resetCurrentLogin);
+  const navigate = useNavigate();
+
+  const onClickLogout = async () => {
+    await revokeSessionGoogle();
+    googleLogout();
+    removeLastLoginButtonClicked();
+    resetCurrentLogin();
+    void navigate('/', { replace: true });
+  };
+
+  return (
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    <button className="logout-button" onClick={onClickLogout}>
+      Logout
+    </button>
+  );
+};
+
+export default GoogleLogOut;

webapp/frontend/src/components/ProtectedRoute.tsx
@@ -1,17 +1,36 @@
-import { InteractionType } from '@azure/msal-browser';
-import { MsalAuthenticationTemplate, useMsal } from '@azure/msal-react';
-import { Navigate, Outlet } from 'react-router';
+import { Navigate, Outlet } from 'react-router-dom';
+import { PATH_WEBAPP } from '../constants/constants';
+import Loading from '../../../../addon/packages/interface/src/components/Loading';
+import { useStoreState } from '../../../../addon/packages/interface/src/hooks/storeHooks';
+import { useVerifyAndRefreshSession } from '../hooks/useVerifyAndRefreshSession';
+import { getLoginGoogleInProgress } from '../helpers/localstorageHelpers';
 
 function ProtectedRoute() {
-  const { accounts } = useMsal();
+  const loginInProgress = getLoginGoogleInProgress();
 
-  const isAuthenticated = accounts.length > 0;
+  const currentLogin = useStoreState((state) => state.auth.currentLogin);
 
-  return (
-    <MsalAuthenticationTemplate interactionType={InteractionType.Redirect}>
-      {isAuthenticated ? <Outlet /> : <Navigate to="/" replace />}
-    </MsalAuthenticationTemplate>
-  );
+  const { accountsMS, inProgressMS, verifyInProgessGoogle, lastClickedLogin } = useVerifyAndRefreshSession({
+    loginGoogleInProgress: loginInProgress,
+  });
+
+  // Wait for MSAL and verifyAndRefreshSessionGoogle to finish processing before checking authentication
+  // Checking loginInProgress === 'cleanedup' is for handling user navigating away from Google login before done
+  // This leaves localstorage key loginGoogleInprogress set to 'true'. Hook useVerifyAndRefreshSession detects this
+  // edge case and sets it to 'cleanup' to show Loading component until verification of authentication is done
+  if (
+    inProgressMS !== 'none' ||
+    ((loginInProgress === 'cleanedup' || verifyInProgessGoogle) && lastClickedLogin === 'Google')
+  ) {
+    return <Loading showSpinner={false} />;
+  }
+
+  const isAuthenticatedMS = accountsMS.length > 0 && lastClickedLogin === 'MS';
+  const isAuthenticatedGoogle = currentLogin.provider === 'Google' && lastClickedLogin === 'Google';
+
+  const isAuthenticated = isAuthenticatedMS || isAuthenticatedGoogle;
+
+  return isAuthenticated ? <Outlet /> : <Navigate to={`/${PATH_WEBAPP}`} replace />;
 }
 
 export default ProtectedRoute;

ws333/iases3frontends

webapp/frontend/src/auth/authConfigMS.ts
@@ -1,8 +1,5 @@
 import { Configuration, LogLevel, PopupRequest, RedirectRequest } from '@azure/msal-browser';
-import { PATH_WEBAPP, SERVER_DEV, SERVER_PROD } from '../constants/constants';
-
-const redirectUri = import.meta.env.PROD ? `${SERVER_PROD}/${PATH_WEBAPP}` : `${SERVER_DEV}/${PATH_WEBAPP}`;
-export const postLogoutRedirectUri = import.meta.env.PROD ? SERVER_PROD : SERVER_DEV;
+import { POST_LOGOUT_REDIRECT_URI, REDIRECT_URI } from '../constants/constantsImportMeta';
 
 /**
  * Configuration object to be passed to MSAL instance on creation.
@@ -13,8 +10,8 @@ export const msalConfig: Configuration = {
   auth: {
     clientId: '3b48599d-a38e-4b59-9504-cbff748b9cc0',
     authority: 'https://login.microsoftonline.com/common',
-    redirectUri: redirectUri, // Points to window.location.origin. You must register this URI on Microsoft Entra admin center/App Registration.
-    postLogoutRedirectUri: postLogoutRedirectUri, // Indicates the page to navigate after logout. You must register this URI on Microsoft Entra admin center/App Registration.
+    redirectUri: REDIRECT_URI, // You must register this URI on Microsoft Entra ID admin center/App Registration.
+    postLogoutRedirectUri: POST_LOGOUT_REDIRECT_URI, // Indicates the page to navigate after logout. You must register this URI on Microsoft Entra admin center/App Registration.
     navigateToLoginRequestUrl: false, // If "true", will navigate back to the original request location before processing the auth code response.
   },
   cache: {

webapp/frontend/src/components/GoogleLogOut.tsx
@@ -1,0 +1,27 @@
+import { googleLogout } from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
+import { useStoreActions } from '../../../../addon/packages/interface/src/hooks/storeHooks';
+import { removeLastLoginButtonClicked } from '../helpers/localstorageHelpers';
+import { revokeSessionGoogle } from '../helpers/revokeSessionGoogle';
+
+const GoogleLogOut = () => {
+  const resetCurrentLogin = useStoreActions((state) => state.auth.resetCurrentLogin);
+  const navigate = useNavigate();
+
+  const onClickLogout = async () => {
+    await revokeSessionGoogle();
+    googleLogout();
+    removeLastLoginButtonClicked();
+    resetCurrentLogin();
+    void navigate('/', { replace: true });
+  };
+
+  return (
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    <button className="logout-button" onClick={onClickLogout}>
+      Logout
+    </button>
+  );
+};
+
+export default GoogleLogOut;

webapp/frontend/src/components/ProtectedRoute.tsx
@@ -1,17 +1,36 @@
-import { InteractionType } from '@azure/msal-browser';
-import { MsalAuthenticationTemplate, useMsal } from '@azure/msal-react';
-import { Navigate, Outlet } from 'react-router';
+import { Navigate, Outlet } from 'react-router-dom';
+import { PATH_WEBAPP } from '../constants/constants';
+import Loading from '../../../../addon/packages/interface/src/components/Loading';
+import { useStoreState } from '../../../../addon/packages/interface/src/hooks/storeHooks';
+import { useVerifyAndRefreshSession } from '../hooks/useVerifyAndRefreshSession';
+import { getLoginGoogleInProgress } from '../helpers/localstorageHelpers';
 
 function ProtectedRoute() {
-  const { accounts } = useMsal();
+  const loginGoogleInProgress = getLoginGoogleInProgress();
 
-  const isAuthenticated = accounts.length > 0;
+  const currentLogin = useStoreState((state) => state.auth.currentLogin);
 
-  return (
-    <MsalAuthenticationTemplate interactionType={InteractionType.Redirect}>
-      {isAuthenticated ? <Outlet /> : <Navigate to="/" replace />}
-    </MsalAuthenticationTemplate>
-  );
+  const { accountsMS, inProgressMS, verifyInProgessGoogle, lastClickedLogin } = useVerifyAndRefreshSession({
+    loginGoogleInProgress,
+  });
+
+  // Wait for MSAL and verifyAndRefreshSessionGoogle to finish processing before checking authentication
+  // Checking loginInProgress === 'cleanedup' is for handling user navigating away from Google login before done
+  // This leaves localstorage key loginGoogleInprogress set to 'true'. Hook useVerifyAndRefreshSession detects this
+  // edge case and sets it to 'cleanup' to show Loading component until verification of authentication is done
+  if (
+    inProgressMS !== 'none' ||
+    ((loginGoogleInProgress === 'cleanedup' || verifyInProgessGoogle) && lastClickedLogin === 'Google')
+  ) {
+    return <Loading showSpinner={false} />;
+  }
+
+  const isAuthenticatedMS = accountsMS.length > 0 && lastClickedLogin === 'MS';
+  const isAuthenticatedGoogle = currentLogin.provider === 'Google' && lastClickedLogin === 'Google';
+
+  const isAuthenticated = isAuthenticatedMS || isAuthenticatedGoogle;
+
+  return isAuthenticated ? <Outlet /> : <Navigate to={`/${PATH_WEBAPP}`} replace />;
 }
 
 export default ProtectedRoute;