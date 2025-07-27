ea038ea1e1521fe7a31725de344eea4b5f230662
Bjørnar Hvidsten <devenania@proton.me>
2025-07-27 16:54:05 +0200

    Add button in LogIn to navigate to docs

--- webapp/frontend/src/constants/constantsImportMeta.ts
+++ webapp/frontend/src/constants/constantsImportMeta.ts
@@ -2,8 +2,19 @@
  * Separate file for contants using import.meta as they cause vite bulid error when in
  * constants.ts which is imported by vite.config.ts
  */
-import { PATH_BASE, PATH_REDIRECT, SERVER_DEV, SERVER_PROD, URL_BACKEND_DEV, URL_BACKEND_PROD } from './constants';
+import {
+  PATH_BASE,
+  PATH_REDIRECT,
+  SERVER_DEV,
+  SERVER_PROD,
+  URL_BACKEND_DEV,
+  URL_BACKEND_PROD,
+  URL_DOCS_DEV,
+  URL_DOCS_PROD,
+} from './constants';
 import { PATH_SEND_EMAIL_GOOGLE, PATH_SEND_EMAIL_MS } from './constantsEndpointPaths';
+
+export const DOCS_URL = import.meta.env.PROD ? URL_DOCS_PROD : URL_DOCS_DEV;
 
 export const REDIRECT_URI = import.meta.env.PROD ? `${SERVER_PROD}/${PATH_REDIRECT}` : `${SERVER_DEV}/${PATH_REDIRECT}`;
 
