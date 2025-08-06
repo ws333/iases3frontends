a2da5fd41145b02a2ad146f9475757823499eda5
Bjørnar Hvidsten <devenania@proton.me>
2025-08-06 17:34:07 +0200

    Display documentation in an iframe

---
+++ addon/packages/interface/src/components/MenuOptionViewOnlineDocumentation.tsx
@@ -0,0 +1,26 @@
+import { ListIcon, MenuOption, MenuView } from "ui-kit";
+import { showDocumentationDialog } from "../helpers/showDocumentationDialog";
+import { useStoreActions } from "../store/store";
+
+type Props = {
+    view: MenuView;
+    onClose: () => void;
+    docsUrl: string;
+};
+
+const MenuOptionViewOnlineDocumentation = ({ view, onClose }: Props) => {
+    const setUserDialog = useStoreActions((actions) => actions.userDialog.setUserDialog);
+
+    const onSelectMenuOption = () => {
+        showDocumentationDialog({ setUserDialog });
+        onClose();
+    };
+
+    return (
+        <div>
+            <MenuOption view={view} text={"View documentation"} onSelect={onSelectMenuOption} icon={<ListIcon />} />
+        </div>
+    );
+};
+
+export default MenuOptionViewOnlineDocumentation;
