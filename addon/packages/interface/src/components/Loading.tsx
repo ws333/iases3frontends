56177f815e0f5ee91ccf7960d8923ce150172ab0
Bjørnar Hvidsten <devenania@proton.me>
2025-08-10 15:52:00 +0200

    Prevent header in Loading from moving when Spinner appear/disappear

--- addon/packages/interface/src/components/Loading.tsx
+++ addon/packages/interface/src/components/Loading.tsx
@@ -1,16 +1,7 @@
-import { CSSProperties } from "react";
 import { Spinner } from "ui-kit";
 import { Text } from "ui-kit";
+import { divContainerButtons, outerDivStyles, spinnerContainerStyles, spinnerHeight } from "../styles/loadingStyles";
 import HeaderWithIFO from "./HeaderWithIFO";
-
-const divStyles: CSSProperties = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    height: "100vh",
-    width: "100%",
-};
 
 type Props = {
     showSpinner?: boolean;
@@ -18,13 +9,14 @@ type Props = {
 
 function Loading({ showSpinner = true }: Props) {
     return (
-        <>
-            <div style={divStyles}>
+        <div style={outerDivStyles}>
+            <div style={divContainerButtons}>
                 <HeaderWithIFO />
-                <br />
-                <Text size={42}>{showSpinner && <Spinner />}</Text>
+                <div style={spinnerContainerStyles}>
+                    <Text size={spinnerHeight}>{showSpinner ? <Spinner /> : null}</Text>
+                </div>
             </div>
-        </>
+        </div>
     );
 }
 
