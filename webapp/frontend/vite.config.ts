62ddd00f2ce0190b5c56beb1f9dc1ce3be0078fd
Bjørnar Hvidsten <devenania@proton.me>
2025-08-18 15:47:13 +0200

    Fix Vite build errors for files outside webapp

--- webapp/frontend/vite.config.ts
+++ webapp/frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     port: PORT_DEV_WEBAPP,
     fs: {
-      allow: [path.resolve(__dirname)],
+      allow: [path.resolve(__dirname), path.resolve(__dirname, '../../addon/packages/interface')],
     },
   },
   resolve: {
