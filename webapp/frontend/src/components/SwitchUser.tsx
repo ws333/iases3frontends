8e985873d9a229878ea0a2d10e4cd051b7b21d91
Bjørnar Hvidsten <devenania@proton.me>
2025-08-21 16:32:15 +0200

    Fix active login buttons not shown if navigating with SwitchUser button when access token expired

--- webapp/frontend/src/components/SwitchUser.tsx
+++ webapp/frontend/src/components/SwitchUser.tsx
@@ -1,16 +1,9 @@
-import { useNavigate } from 'react-router-dom';
 import { Button } from 'ui-kit';
-import { NavigateState } from '../types/types';
-import { useStoreActions } from '../store/storeWithHooks';
+import { URL_WEBAPP_BASE } from '../constants/constantsImportMeta';
 
 const SwitchUser = () => {
-  const navigate = useNavigate();
-
-  const resetCurrentLogin = useStoreActions((state) => state.auth.resetCurrentLogin);
-
   const onClickSwitchUser = () => {
-    resetCurrentLogin();
-    void navigate('/', { replace: true, state: { refresh: true } as NavigateState });
+    window.location.assign(URL_WEBAPP_BASE);
   };
 
   return <Button onClick={onClickSwitchUser}>Switch user</Button>;
