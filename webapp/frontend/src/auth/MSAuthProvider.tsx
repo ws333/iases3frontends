3d7ec026bfd05735e3f92633cbca1931b3a71f32
Bjørnar Hvidsten <devenania@proton.me>
2025-08-22 18:22:56 +0200

    Persist MS logins between browser restarts

--- webapp/frontend/src/auth/MSAuthProvider.tsx
+++ webapp/frontend/src/auth/MSAuthProvider.tsx
@@ -1,5 +1,6 @@
 import { AuthenticationResult, EventMessage, EventType, PublicClientApplication } from '@azure/msal-browser';
 import { MsalProvider } from '@azure/msal-react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { msalConfig } from './authConfigMS';
 
 interface AuthProviderProps {
@@ -11,20 +12,47 @@ export const MSAuthProvider = ({ children }: AuthProviderProps) => {
    * MSAL should be instantiated outside of the component tree to prevent it from being re-instantiated on re-renders.
    * For more, visit: https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-react/docs/getting-started.md
    */
-  const msalInstance = new PublicClientApplication(msalConfig);
+  const msalInstance = useMemo(() => new PublicClientApplication(msalConfig), []);
 
-  if (!msalInstance.getActiveAccount() && msalInstance.getAllAccounts().length > 0) {
-    msalInstance.setActiveAccount(msalInstance.getActiveAccount());
-  }
+  const [initialized, setInitialized] = useState(false);
+
+  useEffect(() => {
+    let isMounted = true;
+    msalInstance
+      .initialize()
+      .then(() => {
+        if (isMounted) {
+          if (!msalInstance.getActiveAccount() && msalInstance.getAllAccounts().length > 0) {
+            msalInstance.setActiveAccount(msalInstance.getActiveAccount());
+          }
+          setInitialized(true);
+        }
+      })
+      .catch((error: unknown) => {
+        console.warn('MSAL initialization error:', (error as Error).message);
+      });
+    return () => {
+      isMounted = false;
+    };
+  }, [msalInstance]);
 
   // Listen for sign-in event and set active account
-  msalInstance.addEventCallback((event: EventMessage) => {
-    const authenticationResult = event.payload as AuthenticationResult | null;
-    const account = authenticationResult?.account;
-    if (event.eventType === EventType.LOGIN_SUCCESS && account) {
-      msalInstance.setActiveAccount(account);
-    }
-  });
+  useEffect(() => {
+    const callbackId = msalInstance.addEventCallback((event: EventMessage) => {
+      const authenticationResult = event.payload as AuthenticationResult | null;
+      const account = authenticationResult?.account;
+      if (event.eventType === EventType.LOGIN_SUCCESS && account) {
+        msalInstance.setActiveAccount(account);
+      }
+    });
+    return () => {
+      if (callbackId) msalInstance.removeEventCallback(callbackId);
+    };
+  }, [msalInstance]);
+
+  if (!initialized) {
+    return null; // Or a loading spinner
+  }
 
   return <MsalProvider instance={msalInstance}>{children}</MsalProvider>;
 };
