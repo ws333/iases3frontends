f73f759c0a7a4e78ca24b695e62f21ee18d18942
Bjørnar Hvidsten <devenania@proton.me>
2025-08-22 20:52:52 +0200

    Fix sendingLog not updated on reset, import, dangeling session
    - Refactor showDeleteHistoryDialog

--- addon/packages/interface/src/helpers/showDeleteHistoryDialog.ts
+++ addon/packages/interface/src/helpers/showDeleteHistoryDialog.ts
@@ -1,27 +1,24 @@
-import { ActionCreator } from "easy-peasy";
 import { toast } from "react-toastify";
-import { UserDialog } from "../types/modelTypes";
 import TextDeletingData from "../components/dialogTexts/TextDeletingData";
+import { store } from "../store/store";
 import { toastOptions } from "../styles/styles";
 import { resetStorage } from "./indexedDB";
 
-type Args = {
-    initiateForcedRender: ActionCreator<void>;
-    setUserDialog: ActionCreator<Partial<UserDialog>>;
-};
+export function showDeleteHistoryDialog() {
+    const actions = store.getActions();
 
-export function showDeleteHistoryDialog({ initiateForcedRender, setUserDialog }: Args) {
-    setUserDialog({
+    actions.userDialog.setUserDialog({
         title: "Deletion warning!",
         message: TextDeletingData,
         confirmActionText: "Confirm deletion",
         onConfirm: async () => {
             const resetOk = await resetStorage();
             if (resetOk) {
+                actions.sendingLog.setLog([]);
+                actions.render.initiateForcedRender();
                 const message = "Sending history has been deleted!";
                 toast(message, toastOptions);
                 console.log(message);
-                initiateForcedRender();
             } else {
                 const message = "Failed to delete history!";
                 toast(message, {
