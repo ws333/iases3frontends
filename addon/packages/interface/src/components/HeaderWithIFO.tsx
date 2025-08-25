a7552f075238ae3d00e08d977e4dbf5a5eb64206
Bjørnar Hvidsten <devenania@proton.me>
2025-08-25 21:14:17 +0200

    Center HeaderWithIFO content

--- addon/packages/interface/src/components/HeaderWithIFO.tsx
+++ addon/packages/interface/src/components/HeaderWithIFO.tsx
@@ -4,15 +4,23 @@ import IconIFO from "./IconIFO";
 
 function HeaderWithIFO() {
     return (
-        <>
+        <div style={outerDivStyles}>
             <IconIFO />
             <div style={divDynamicGapStyles}></div>
             <Header />
-        </>
+        </div>
     );
 }
 
 export default HeaderWithIFO;
+
+const outerDivStyles: CSSProperties = {
+    display: "flex",
+    width: "100%",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+};
 
 const divDynamicGapStyles: CSSProperties = {
     height: "3%",
