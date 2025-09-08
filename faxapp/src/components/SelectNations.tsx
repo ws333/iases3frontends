4ac85701adbbc39769d0362df623c884ad984c48
Bjørnar Hvidsten <devenania@proton.me>
2025-09-09 00:33:50 +0200

    Refactor SelectNations
    - Rename class to avoid collisions
    - Remove obsolete css

--- faxapp/src/components/SelectNations.tsx
+++ faxapp/src/components/SelectNations.tsx
@@ -31,51 +31,47 @@ function SelectNations({ selectedContactsNotSent, isSending }: Props) {
   return (
     <div className="select-nations">
       <div>
-        <div className="title-container">
-          <label className="title">Available contacts</label>
+        <label className="select-nations-label">Available contacts</label>
+        <div className={`nation-list ${isSending ? 'disabled' : ''}`}>
+          <label className="nation-item">
+            <input type="checkbox" disabled={isSending} checked={isSelectedAllNations} onChange={onChangeSelectAll} />
+            Select All
+          </label>
         </div>
-        <>
-          <div className={`nation-list ${isSending ? 'disabled' : ''}`}>
-            <label className="nation-item">
-              <input type="checkbox" disabled={isSending} checked={isSelectedAllNations} onChange={onChangeSelectAll} />
-              Select All
-            </label>
-          </div>
-          <div className={`nation-list-columns ${isSending ? 'disabled' : ''}`}>
-            {nationColumns.map((col, colIdx) => (
-              <div key={colIdx} className="nation-list-column">
-                {col.map((nation) => (
-                  <label key={nation} className="nation-item">
-                    <input
-                      type="checkbox"
-                      disabled={isSending}
-                      onChange={(e) => onChangeNation(e.target.checked, nation)}
-                      checked={selectedNations.includes(nation)}
-                    />
-                    {nation}
-                  </label>
-                ))}
-              </div>
-            ))}
-            {!nationOptions.length && (
-              <div className="nation-list-empty">
-                <p>No contacts are yet available for the country you are currently connected to</p>
-                <p>If using a VPN service connect to a server in the appropriate country</p>
-              </div>
-            )}
-          </div>
-          <div className="selected-info">
-            <div>
-              <div>Selected contacts</div>
-              <div>Not sent to last 3 months</div>
+        <div className={`nation-list-columns ${isSending ? 'disabled' : ''}`}>
+          {nationColumns.map((col, colIdx) => (
+            <div key={colIdx} className="nation-list-column">
+              {col.map((nation) => (
+                <label key={nation} className="nation-item">
+                  <input
+                    type="checkbox"
+                    disabled={isSending}
+                    onChange={(e) => onChangeNation(e.target.checked, nation)}
+                    checked={selectedNations.includes(nation)}
+                  />
+                  {nation}
+                </label>
+              ))}
             </div>
-            <div>
-              <div>{selectedContacts.length}</div>
-              <div>{selectedContactsNotSent.length}</div>
+          ))}
+          {!nationOptions.length && (
+            <div className="nation-list-empty">
+              <p>No contacts are yet available for the country you are currently connected to</p>
+              <p>If using a VPN service connect to a server in the appropriate country</p>
             </div>
+          )}
+        </div>
+        <div className="selected-info">
+          <div>
+            <div>Selected contacts</div>
+            <div>Not sent to last 3 months</div>
           </div>
-          <SentCounts />
-        </>
+          <div>
+            <div>{selectedContacts.length}</div>
+            <div>{selectedContactsNotSent.length}</div>
+          </div>
+        </div>
+        <SentCounts />
       </div>
     </div>
   );
