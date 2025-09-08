fe51d2cf8ff7f054cc480bd549496dec82060c83
Bjørnar Hvidsten <devenania@proton.me>
2025-09-08 23:55:09 +0200

    Update ignore files and properties

--- faxapp/eslint.config.mjs
+++ faxapp/eslint.config.mjs
@@ -6,7 +6,7 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['**/dist/**', '**/_backup/**'] },
+  { ignores: ['**/dist/**'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.strictTypeChecked, ...tseslint.configs.stylisticTypeChecked],
     files: ['**/*.{ts,tsx}'],
