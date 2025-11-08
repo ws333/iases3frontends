40e5bfd916b80842f2bd47e2b5a3229f1c0011b2
Bjørnar Hvidsten <devenania@proton.me>
2025-11-08 15:55:08 +0100

    Set default "Number of faxes to send" to 10 for faxapp

--- faxapp/src/constants/constants.ts
+++ faxapp/src/constants/constants.ts
@@ -51,7 +51,7 @@ export const fullProgressBarDelay = 2;
 export const logsToDisplaySize = 1000;
 
 export const maxCountOptions = [5, 10, 25, 50, 75, 100, 150, 200, 250, 300, 400, 500, 600, 700, 800, 900, 1000];
-export const defaultMaxCount = maxCountOptions[5];
+export const defaultMaxCount = maxCountOptions[1];
 
 export const minPassphraseLength = 12; // Recommended value is 16, but since there is a cap on number of faxes allowed to be sent 12 suffices.
 
